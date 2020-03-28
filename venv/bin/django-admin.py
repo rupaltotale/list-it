9e79fae15dbd334f8c42e3171e5c88ed53cb3e0e
Rupal Totale <rupaltotale@gmail.com>
2020-03-28 15:49:57 -0700

    setup the project backend and frontend

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/rupalt/Desktop/Personal Projects/To-do-list-webapp/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
