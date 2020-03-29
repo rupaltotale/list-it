07fe93faf051032610aa1463abe0236ce6ab6477
YashTotale <totaleyash@gmail.com>
2020-03-28 17:35:16 -0700

    New models

--- leads/models.py
+++ leads/models.py
@@ -2,10 +2,20 @@
 # List: id, title, owner (userId), dateCreated,
 # ListItem: id, listId, completed, content
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Lead(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class List(models.Model):
+    title = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    dateCreated = models.DateTimeField()
+    listItems = models.ManyToManyField('ListItem', related_name='listItems')
+
+class ListItem(models.Model):
+    completed = models.BooleanField()
+    content = models.CharField(max_length=200)
--- leads/serializers.py
+++ leads/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Lead
+from .models import Lead, List, ListItem
 
 class LeadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lead
         fields = ('id', 'name', 'email', 'message')
+
+class ListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = List
+        fields = ('title', 'owner', 'dateCreated', 'listItems')
+
+class ListItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ListItem
+        fields = ('completed', 'content')
