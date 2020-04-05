a0a4da54b99b65ffb27ef1981fa5cefc563e576c
Rupal Totale <rupaltotale@gmail.com>
2020-04-04 21:41:56 -0700

    react router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,118 +1,55 @@
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import React, { Component } from "react";
 
-import LoginForm from "./components/LoginForm";
-import Nav from "./components/Nav";
-import SignupForm from "./components/SignupForm";
 import { render } from "react-dom";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayed_form: "",
-      logged_in: localStorage.getItem("token") ? true : false,
-      username: ""
-    };
-  }
-
-  componentDidMount() {
-    console.log("component mounted");
-    if (this.state.logged_in) {
-      fetch("http://localhost:8000/current_user/", {
-        headers: {
-          Authorization: `JWT ${localStorage.getItem("token")}`
-        }
-      })
-        .then(res => res.json())
-        .then(json => {
-          console.log(json);
-          this.setState({ username: json.username });
-        });
-    }
-  }
-
-  handle_login = (e, data) => {
-    e.preventDefault();
-    fetch("http://localhost:8000/token-auth/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    })
-      .then(res => res.json())
-      .then(json => {
-        localStorage.setItem("token", json.token);
-        this.setState({
-          logged_in: true,
-          displayed_form: "",
-          username: json.user.username
-        });
-      });
-  };
-
-  handle_signup = (e, data) => {
-    e.preventDefault();
-    fetch("http://localhost:8000/users/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    })
-      .then(res => res.json())
-      .then(json => {
-        console.log(json);
-        localStorage.setItem("token", json.token);
-        this.setState({
-          logged_in: true,
-          displayed_form: "",
-          username: json.username
-        });
-      });
-  };
-
-  handle_logout = () => {
-    localStorage.removeItem("token");
-    this.setState({ logged_in: false, username: "" });
-  };
-
-  display_form = form => {
-    this.setState({
-      displayed_form: form
-    });
-  };
-
   render() {
-    let form;
-    switch (this.state.displayed_form) {
-      case "login":
-        form = <LoginForm handle_login={this.handle_login} />;
-        break;
-      case "signup":
-        form = <SignupForm handle_signup={this.handle_signup} />;
-        break;
-      default:
-        form = null;
-    }
-
     return (
-      <div className="App">
-        <Nav
-          logged_in={this.state.logged_in}
-          display_form={this.display_form}
-          handle_logout={this.handle_logout}
-        />
-        {form}
-        <h3>
-          {this.state.logged_in
-            ? `Hello, ${this.state.username}`
-            : "Please Log In"}
-        </h3>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
+function Home() {
+  return <h2>Home</h2>;
+}
 
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 const container = document.getElementById("app");
 render(<App />, container);
